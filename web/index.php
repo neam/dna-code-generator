58c6975e347c7c6549658ebdb40e406ea9bd0b3f
Fredrik Wollsén <fredrik@neam.se>
2015-02-23 01:29:40 +0100

    Removed duplicate require

--- web/index.php
+++ web/index.php
@@ -6,8 +6,6 @@ require(__DIR__ . '/../config/env.php');
 defined('YII_DEBUG') or define('YII_DEBUG', (boolean)getenv('YII_DEBUG'));
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
 
-require(__DIR__ . '/../vendor/autoload.php');
-
 // Define project directory
 $rootPath = __DIR__ . '/..';
 
