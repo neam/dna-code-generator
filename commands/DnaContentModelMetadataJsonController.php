34f3f4b1eeb2179b413e684249b0024098c7679e
Fredrik Wollsén <fredrik@neam.se>
2017-11-14 23:02:24 +0200

    Refactored content model metadata merge logic somewhat, showing what actually takes time

--- commands/DnaContentModelMetadataJsonController.php
+++ commands/DnaContentModelMetadataJsonController.php
@@ -38,6 +38,17 @@ class DnaContentModelMetadataJsonController extends \yii\console\Controller
             $configIds = [$this->configId];
         }
 
+        $fasterArrayMerge = function (&$array1, $array2) {
+            foreach ($array2 as $i) {
+                $array1[] = $i;
+            }
+        };
+
+        $merged = (object) [
+            'itemTypes' => [],
+            'itemTypeAttributes' => [],
+        ];
+
         // First content model metadata exports as usual
         $configId = array_shift($configIds);
         $firstCmm = \ContentModelMetadata::model()->findByPk($configId);
@@ -45,26 +56,38 @@ class DnaContentModelMetadataJsonController extends \yii\console\Controller
             throw new ErrorException("There is no ContentModelMetadata record with id {$configId}");
         }
         $duration = round(microtime(true) - $start, 1);
+        echo "* {$duration}s - Fetching related items for {$firstCmm->getItemLabel()} \n";
+        $merged->itemTypes = $firstCmm->itemTypes;
+        $merged->itemTypeAttributes = $firstCmm->itemTypeAttributes;
         echo "* {$duration}s - Starting with {$firstCmm->getItemLabel()} \n";
 
         // The rest are superimposed on the first content model metadata to create a joint export of the correct format
         if (!empty($configIds)) {
             foreach ($configIds as $configId) {
+                $duration = round(microtime(true) - $start, 1);
+                echo "* {$duration}s - Fetching config with id {$configId} \n";
                 $cmm = \ContentModelMetadata::model()->findByPk($configId);
                 if (empty($cmm)) {
                     throw new ErrorException("There is no ContentModelMetadata record with id {$configId}");
                 }
                 $duration = round(microtime(true) - $start, 1);
+                echo "* {$duration}s - Fetching related items for {$cmm->getItemLabel()} \n";
+                $itemTypes = $cmm->itemTypes;
+                $itemTypeAttributes = $cmm->itemTypeAttributes;
                 echo "* {$duration}s - Merging in {$cmm->getItemLabel()} \n";
                 // Union item types and attributes, which only should be specified in one cmm at once
-                $firstCmm->itemTypes = array_merge($firstCmm->itemTypes, $cmm->itemTypes);
-                $firstCmm->itemTypeAttributes = array_merge($firstCmm->itemTypeAttributes, $cmm->itemTypeAttributes);
+                $fasterArrayMerge($merged->itemTypes, $itemTypes);
+                $fasterArrayMerge($merged->itemTypeAttributes, $itemTypeAttributes);
             }
         }
 
         $duration = round(microtime(true) - $start, 1);
-        echo "* {$duration}s - Exporting JSON...\n";
-        $json = Json::encode($firstCmm->export());
+        echo "* {$duration}s - Exporting merged metadata...\n";
+
+        $export = \ContentModelMetadata::exportStatic($merged->itemTypes, $merged->itemTypeAttributes);
+
+        echo "* {$duration}s - Encoding JSON...\n";
+        $json = Json::encode($export);
         file_put_contents('/app/out.json', $json);
 
         echo "* {$duration}s - Done. Contents (len " . strlen($json) . ") saved in out.json\n";
