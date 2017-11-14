9deabb418608c8b30ccd88d649b65602b828cff5
Fredrik Wollsén <fredrik@neam.se>
2017-11-14 21:14:16 +0200

    Log output during json composition and export

--- commands/DnaContentModelMetadataJsonController.php
+++ commands/DnaContentModelMetadataJsonController.php
@@ -29,6 +29,8 @@ class DnaContentModelMetadataJsonController extends \yii\console\Controller
     {
         require(DNA_PROJECT_PATH . "/dna/models/ContentModelMetadata.php");
 
+        $start = microtime(true);
+
         // Allow comma-separated list of config ids to include in export
         if (strpos($this->configId, ",") !== false) {
             $configIds = explode(",", $this->configId);
@@ -42,6 +44,8 @@ class DnaContentModelMetadataJsonController extends \yii\console\Controller
         if (empty($firstCmm)) {
             throw new ErrorException("There is no ContentModelMetadata record with id {$configId}");
         }
+        $duration = round(microtime(true) - $start, 1);
+        echo "* {$duration}s - Starting with {$firstCmm->getItemLabel()} \n";
 
         // The rest are superimposed on the first content model metadata to create a joint export of the correct format
         if (!empty($configIds)) {
@@ -50,13 +54,20 @@ class DnaContentModelMetadataJsonController extends \yii\console\Controller
                 if (empty($cmm)) {
                     throw new ErrorException("There is no ContentModelMetadata record with id {$configId}");
                 }
+                $duration = round(microtime(true) - $start, 1);
+                echo "* {$duration}s - Merging in {$cmm->getItemLabel()} \n";
                 // Union item types and attributes, which only should be specified in one cmm at once
                 $firstCmm->itemTypes = array_merge($firstCmm->itemTypes, $cmm->itemTypes);
                 $firstCmm->itemTypeAttributes = array_merge($firstCmm->itemTypeAttributes, $cmm->itemTypeAttributes);
             }
         }
 
-        echo Json::encode($firstCmm->export());
+        $duration = round(microtime(true) - $start, 1);
+        echo "* {$duration}s - Exporting JSON...\n";
+        $json = Json::encode($firstCmm->export());
+        file_put_contents('/app/out.json', $json);
+
+        echo "* {$duration}s - Done. Contents (len " . strlen($json) . ") saved in out.json\n";
     }
 
 }
